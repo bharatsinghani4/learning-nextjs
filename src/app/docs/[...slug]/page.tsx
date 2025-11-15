f59670e4ac237ddba616526668ac1f2192e9602a
Bharat Singhani <singhani.bharat7@gmail.com>
2025-11-16 01:33:22 +0530

    Catch All Segments

---
+++ src/app/docs/[...slug]/page.tsx
@@ -0,0 +1,17 @@
+const Docs = async ({ params }: { params: Promise<{ slug: string[] }> }) => {
+  const { slug } = await params;
+
+  if (slug?.length === 2) {
+    return (
+      <h1>
+        Viewing docs for {slug[0]} and {slug[1]}
+      </h1>
+    );
+  } else if (slug?.length === 1) {
+    return <h1>Viewing docs for {slug[0]}</h1>;
+  }
+
+  return <h1>Docs page</h1>;
+};
+
+export default Docs;
